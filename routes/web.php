1f5e4f79a473b4ce23e57fdaa50b05cc485508cb
achmadfauzi13 <ujiahmadfauzi13@gmail.com>
2024-06-12 22:31:45 +0700

    Pertemuan-13

--- routes/web.php
+++ routes/web.php
@@ -1,41 +1,42 @@
 <?php
 
 use App\Http\Controllers\MovieController;
-use App\Http\Controllers\ProductController;
 use App\Http\Controllers\GenreController;
 use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/products', [ProductController::class, 'index']);
+Route::resource('/movies', MovieController::class);
+Route::get('/products', [ProductController::class,'index']);
 
-Route::get('/movies', [MovieController::class, 'index']);
-Route::get('/movies/create', [MovieController::class, 'create']);
-Route::post('/movies', [MovieController::class, 'store']);
-Route::get('/movies/{movie}/edit', [MovieController::class, 'edit']);
-Route::put('/movies/{movie}', [MovieController::class, 'update']);
-Route::delete('/movies/{movie}', [MovieController::class, 'destroy']);
+Route::get('/dashboard', function () {
+    return view('welcome');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/genres', [GenreController::class, 'index']);
-Route::get('/genres/create', [GenreController::class, 'create']);
-Route::post('/genres', [GenreController::class, 'store']);
-Route::get('/genres/{genre}/edit', [GenreController::class, 'edit']);
-Route::put('/genres/{genre}', [GenreController::class, 'update']);
-Route::delete('/genres/{genre}', [GenreController::class, 'destroy']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-Route::get('/reviews', [ReviewController::class, 'index']);
-Route::get('/reviews/create', [ReviewController::class, 'create']);
-Route::post('/reviews', [ReviewController::class, 'store']);
-Route::get('/reviews/{review}/edit', [ReviewController::class, 'edit']);
-Route::put('/reviews/{review}', [ReviewController::class, 'update']);
-Route::delete('/reviews/{review}', [ReviewController::class, 'destroy']);
+    Route::resource('/movies', MovieController::class);
+    Route::resource('/genres', GenreController::class);
+    Route::resource('/reviews', ReviewController::class);
+});
 
-
-
-
-// Route::get('/si02', function () {
-//     return "belajar laravel";
-// });
+require __DIR__.'/auth.php';
